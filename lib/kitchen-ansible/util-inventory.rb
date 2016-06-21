7885769c4b72b24241de42cf8a480a1fa8aed24c
Mohamed Ghaleb <mohamed.ghaleb@gmail.com>
2016-06-21 23:02:29 +0200

    Initial support for windows. Tested on azure

--- lib/kitchen-ansible/util-inventory.rb
+++ lib/kitchen-ansible/util-inventory.rb
@@ -3,13 +3,14 @@ TEMP_GROUP_FILE = "#{TEMP_INV_DIR}/ansiblepush_groups_inventory.yml"
 
 
 
-def write_instance_inventory(name, host, mygroup, instance_connection_option)
+def write_instance_inventory(name, host, mygroup, instance_connection_option, ansible_connection)
   Dir.mkdir TEMP_INV_DIR if !File.exist?(TEMP_INV_DIR)
-  
+
   unless instance_connection_option.nil?
         port = instance_connection_option[:port]
         keys = instance_connection_option[:keys]
         user = instance_connection_option[:user]
+        pass = instance_connection_option[:pass]
   end
 
   temp_hash = Hash.new
@@ -17,7 +18,13 @@ def write_instance_inventory(name, host, mygroup, instance_connection_option)
   temp_hash["ansible_ssh_port"] = port if port
   temp_hash["ansible_ssh_private_key_file"] = keys[0] if keys
   temp_hash["ansible_ssh_user"] = user if user
+  temp_hash["ansible_ssh_pass"] = pass if pass
   temp_hash["mygroup"] = mygroup if mygroup
+  # WIndows issue ignore SSL
+  if ansible_connection == "winrm"
+    temp_hash["ansible_winrm_server_cert_validation"] = "ignore"
+    temp_hash["ansible_winrm_transport"] = "ssl"
+  end
 
   host = { name => temp_hash }
   File.open("%s/ansiblepush_host_%s.yml" % [TEMP_INV_DIR, name], "w") do |file|
