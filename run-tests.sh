de6493ef7adec84ecaa96e310d437b8fc4fd02d4
Adham Helal <aa@hellofresh.com>
2016-07-03 00:50:15 +0200

    remove whitespace

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ bundle exec rspec --require spec_helper --format d
 bundle exec kitchen test simple
 bundle exec kitchen test notidempotent | tee /tmp/notidempotent
 
-failed_nonotidempotent=$(cat /tmp/notidempotent | grep "idempotency test \[Failed\]" | wc -l)
+failed_nonotidempotent=$(cat /tmp/notidempotent | grep "idempotency test \[Failed\]" | wc -l |  tr -d '[[:space:]]')
 if [ ! "${failed_nonotidempotent}" == "2" ] ; then 
     echo "Non idempotent tasks $failed_nonotidempotent. :( failed" 
     exit 1 
