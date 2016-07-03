0f85f650b9d455aa8c9430be794ead8c40a01e47
Adham Helal <aa@hellofresh.com>
2016-07-03 11:58:25 +0200

    Split chef installation into a different directory

--- lib/kitchen/provisioner/ansible_push.rb
+++ lib/kitchen/provisioner/ansible_push.rb
@@ -2,6 +2,7 @@ require 'open3'
 require 'kitchen'
 require 'kitchen/provisioner/base'
 require 'kitchen-ansible/util_inventory.rb'
+require 'kitchen-ansible/chef_installation.rb'
 require 'json'
 require 'securerandom'
 
@@ -161,32 +162,8 @@ module Kitchen
       def chef_installation(chef_url, omnibus_download_dir, transport)
         if chef_url
           scripts = []
-
           scripts << Util.shell_helpers
-
-          scripts << <<-INSTALL
-            if [ ! -d "/opt/chef" ]
-            then
-              echo "-----> Installing Chef Omnibus needed by busser and serverspec"
-              mkdir -p #{omnibus_download_dir}
-              if [ ! -x #{omnibus_download_dir}/install.sh ]
-              then
-                do_download #{chef_url} #{omnibus_download_dir}/install.sh
-              fi
-              sudo sh #{omnibus_download_dir}/install.sh -d #{omnibus_download_dir}
-              echo "-----> End Installing Chef Omnibus"
-            fi
-          INSTALL
-
-          scripts << <<-INSTALL
-            # Fix for https://github.com/test-kitchen/busser/issues/12
-            if [ -h /usr/bin/ruby ]; then
-                L=$(readlink -f /usr/bin/ruby)
-                sudo rm /usr/bin/ruby
-                sudo ln  -s $L /usr/bin/ruby
-            fi
-          INSTALL
-
+          scripts << chef_installation_script(chef_url, omnibus_download_dir, transport)
           <<-INSTALL
             sh -c '#{scripts.join("\n")}'
           INSTALL
