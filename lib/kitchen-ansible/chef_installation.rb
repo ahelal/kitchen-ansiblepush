5a74dc86dc293b7a3cedfdcaf05b2aa022570555
Mohamed Ghaleb <mohamed.ghaleb@gmail.com>
2016-07-17 17:13:23 +0200

    Adding the support of the Chef install.ps1 script auto install for windows

--- lib/kitchen-ansible/chef_installation.rb
+++ lib/kitchen-ansible/chef_installation.rb
@@ -1,16 +1,27 @@
 
-def chef_installation_script(chef_url, omnibus_download_dir, transport)
-  <<-INSTALL
-      if [ ! -d "/opt/chef" ]
-      then
-        echo "-----> Installing Chef Omnibus needed by busser and serverspec"
-        mkdir -p #{omnibus_download_dir}
-        if [ ! -x #{omnibus_download_dir}/install.sh ]
+def chef_installation_script_linux(chef_url, omnibus_download_dir)
+    <<-INSTALL
+        if [ ! -d "/opt/chef" ]
         then
-          do_download #{chef_url} #{omnibus_download_dir}/install.sh
+          echo "-----> Installing Chef Omnibus needed by busser and serverspec"
+          mkdir -p #{omnibus_download_dir}
+          if [ ! -x #{omnibus_download_dir}/install.sh ]
+          then
+            do_download #{chef_url} #{omnibus_download_dir}/install.sh
+          fi
+          sudo sh #{omnibus_download_dir}/install.sh -d #{omnibus_download_dir}
+          echo "-----> End Installing Chef Omnibus"
         fi
-        sudo sh #{omnibus_download_dir}/install.sh -d #{omnibus_download_dir}
-        echo "-----> End Installing Chef Omnibus"
-      fi
-  INSTALL
+    INSTALL
+end
+
+def chef_installation_script_windows(chef_url)
+    <<-INSTALL
+    if((Test-Path "c:\\opscode\\chef\\bin") -eq 0)
+    {
+      echo "-----> Installing Chef Omnibus needed by busser and serverspec"
+      . { iwr -useb #{chef_url} } | iex; install -channel current -project chef
+      echo "-----> End Installing Chef Omnibus"
+    }
+    INSTALL
 end
