c6b3c8199b05e82d6b55fc58defbf6f156014abd
Adham Helal <aa@hellofresh.com>
2017-01-01 16:18:55 +0100

    House cleaning gem and rubicon

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -9,14 +9,13 @@ echo "Run rspec"
 bundle exec rspec --require spec_helper --format d
 
 echo "Run Rubocop"
-bundle exec rubocop lib bin
+bundle exec rubocop lib bin spec
 
 bundle exec kitchen test simpleNoChef
 bundle exec kitchen test simpleWithChef
 bundle exec kitchen test notidempotent | tee /tmp/notidempotent
 
 number_of_servers_in_idempotent="$(bundle exec kitchen list notidempotent | grep Ssh | wc -l |  tr -d '[[:space:]]')"
-#number_of_servers_in_idempotent="1"
 
 failed_nonotidempotent=$(cat /tmp/notidempotent | grep "Warning idempotency test \[failed\]" | wc -l |  tr -d '[[:space:]]')
 if [ ! "${failed_nonotidempotent}" = "${number_of_servers_in_idempotent}" ] ; then
