cd00e2c616958a8008bc16aaac29279c61714123
Adham <ahelal@users.noreply.github.com>
2018-04-30 23:36:25 +0200

    add more ansible test (#49)
    * add more ansible test
    
    * fix python 2 issue and upgrade some dep

--- lib/kitchen-ansible/version.rb
+++ lib/kitchen-ansible/version.rb
@@ -1,5 +1,5 @@
 module Kitchen
   module AnsiblePush
-    VERSION = '0.8.0'.freeze
+    VERSION = '0.9.1'.freeze
   end
 end
--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -24,9 +24,13 @@ export ANSIBLE_VERSIONS_4="2.3.2.0"
 export INSTALL_TYPE_4="pip"
 export ANSIBLE_LABEL_4="v2.3"
 
-export ANSIBLE_VERSIONS_5="2.4.0.0"
+export ANSIBLE_VERSIONS_5="2.4.2.0"
 export INSTALL_TYPE_5="pip"
 export ANSIBLE_LABEL_5="v2.4"
+
+export ANSIBLE_VERSIONS_6="2.5.0"
+export INSTALL_TYPE_6="pip"
+export ANSIBLE_LABEL_6="v2.5"
 
 # Whats the default version
 export ANSIBLE_DEFAULT_VERSION="v1.9"
